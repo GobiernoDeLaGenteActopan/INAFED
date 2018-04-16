ba2a06e71e3ea9fae58bf45ebecbb69b1713b871
Martin Melo <nitram-210397@hotmail.com>
2018-04-16 12:34:06 -0500

    Added style to errors in Login

--- INADEF/settings.py
+++ INADEF/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
-    'accounts'
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +129,5 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = '/'
--- INADEF/urls.py
+++ INADEF/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import LoginView, logout
 from main.views import Home
 
 urlpatterns = [
 	path('', Home.as_view(), name="home"),
-    path('login/', login, name="login"),
+    path('login/', LoginView.as_view(), name="login"),
     path('logout/', logout, name="logout"),
     path('admin/', admin.site.urls),
 ]
