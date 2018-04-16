ec5b35a14ef54b872e2d693030537a478660c7ee
Martin Melo <nitram-210397@hotmail.com>
2018-04-16 14:04:39 -0500

    Create models of indicadores app

--- indicadores/models.py
+++ indicadores/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+choices_nivel = (
+        'Gestión',
+        'Desempeño',
+    )
+
+choices_status = (
+    ('#2ecc71', 'Satisfactorio'),
+    ('#2ecc71', 'Regular'),
+    ('#2ecc71', 'Pésimo')
+)
+
+
+class Indicador(models.Model):
+
+    indicador = models.CharField(max_length=10)
+    nombre = models.TextField()
+    descripcion = models.TextField(blank=True, null=True)
+    nivel = models.CharField(max_length=15, choices=choices_nivel, blank=True, null=True)
+    dimension = models.CharField(max_length=144, blank=True, null=True)
+    criterios_evaluacion = models.TextField(blank=True, null=True)
+    area = models.ForeignKey(User, related_name='indicadores', on_delete=models.CASCADE)
+    status = models.CharField(max_length=15, choices=choices_status)
+    created = models.DateTimeField(auto_created=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.indicador + ' - ' + self.nombre
+
+
+class Evidencia(models.Model):
+
+    indicador = models.ForeignKey(Indicador, related_name="evidencias", on_delete=models.CASCADE)
+    titulo = models.TextField()
+    porcentaje = models.DecimalField(blank=True, null=True, max_digits=3, decimal_places=2)
+    archivo = models.FileField(upload_to='evidencias/', blank=True, null=True)
+    completada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.indicador.indicador + ' - ' + self.titulo
+
+
+class Subevidencia(models.Model):
+
+    evidencia = models.ForeignKey(Evidencia, related_name="subevidencias", on_delete=models.CASCADE)
+    titulo = models.TextField()
+    porcentaje = models.DecimalField(blank=True, null=True, max_digits=3, decimal_places=2)
+    archivo = models.FileField(upload_to='evidencias/', blank=True, null=True)
+    completada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.indicador.indicador + ' - ' + self.evidencia.titulo + ' - ' + self.titulo
+
+
+class Seguimiento(models.Model):
+
+    indicador = models.ForeignKey(Indicador, related_name="seguimientos", on_delete=models.CASCADE)
+    cuerpo = models.TextField()
+    fecha = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.indicador.indicador + ' - ' + self.cuerpo
+
