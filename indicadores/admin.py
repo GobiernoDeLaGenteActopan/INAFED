52183fa8fda59aff688aa458c97d72f7e862a981
Martin Melo <nitram-210397@hotmail.com>
2018-07-25 11:42:08 -0500

    Acciones para cualquier status en el admin

--- indicadores/admin.py
+++ indicadores/admin.py
@@ -5,11 +5,24 @@ from .models import Indicador, Evidencia, Subevidencia, Seguimiento
 class IndicadorAdmin(admin.ModelAdmin):
     list_display = ['indicador', 'nombre', 'status']
     list_filter = ['status']
-    actions = ['make_insatisfactorio']
+    actions = ['make_insatisfactorio', 'make_satisfactorio', 'make_regular', 'make_ncs']
 
     def make_insatisfactorio(self, request, queryset):
         queryset.update(status="Insatisfactorio")
-    make_insatisfactorio.short_description = "Colocar como status insatisfactorio"
+
+    def make_satisfactorio(self, request, queryset):
+        queryset.update(status="Satisfactorio")
+
+    def make_regular(self, request, queryset):
+        queryset.update(status="Regular")
+
+    def make_ncs(self, request, queryset):
+        queryset.update(status="NCS")
+
+    make_insatisfactorio.short_description = "Colocar como Insatisfactorio"
+    make_satisfactorio.short_description = "Colocar como Satisfactorio"
+    make_regular.short_description = "Colocar como Regular"
+    make_ncs.short_description = "Colocar como NCS"
 
 admin.site.register(Indicador, IndicadorAdmin)
 #admin.site.register(Evidencia)
