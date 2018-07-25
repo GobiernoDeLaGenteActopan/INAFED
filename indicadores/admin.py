9121972fd50aeccf170fb313878869b1afb202d8
Martin Melo <nitram-210397@hotmail.com>
2018-07-25 11:29:04 -0500

    Admin mejorado

--- indicadores/admin.py
+++ indicadores/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Indicador, Evidencia, Subevidencia, Seguimiento
 
-admin.site.register(Indicador)
+
+class IndicadorAdmin(admin.ModelAdmin):
+    list_display = ['indicador', 'nombre', 'status']
+    list_filter = ['status']
+
+admin.site.register(Indicador, IndicadorAdmin)
 #admin.site.register(Evidencia)
 #admin.site.register(Subevidencia)
 #admin.site.register(Seguimiento)
