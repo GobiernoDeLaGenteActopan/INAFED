c1933cdb032e78e46cf5d4c1c6b5ec19e9a9b34c
Martin Melo <nitram-210397@hotmail.com>
2018-07-25 11:35:51 -0500

    Accion en el admin de django para pasar todos lo seleccionados a insatisfactorio

--- indicadores/admin.py
+++ indicadores/admin.py
@@ -5,6 +5,11 @@ from .models import Indicador, Evidencia, Subevidencia, Seguimiento
 class IndicadorAdmin(admin.ModelAdmin):
     list_display = ['indicador', 'nombre', 'status']
     list_filter = ['status']
+    actions = ['make_insatisfactorio']
+
+    def make_insatisfactorio(self, request, queryset):
+        queryset.update(status="Insatisfactorio")
+    make_insatisfactorio.short_description = "Colocar como status insatisfactorio"
 
 admin.site.register(Indicador, IndicadorAdmin)
 #admin.site.register(Evidencia)
